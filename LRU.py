d44609620885252554ca33b33175453f1b84caa3
allurivamsi5 <allurivamsi5@msitprogram.net>
2020-04-14 11:02:12 +0530

    added public methods

--- LRU.py
+++ LRU.py
@@ -2,3 +2,9 @@ class LRU:
     def __init__(self, cache_size):
         self.cache_size = cache_size
         self.cache = []
+    def set(self, num):
+        pass
+    def get(self, num):
+        pass
+    def get_cache(self):
+        pass
