ab3168af2670d97b94166c8ec4845ac7787795e7
allurivamsi5 <allurivamsi5@msitprogram.net>
2020-04-14 20:55:59 +0530

    created LRUTest class

--- LRU.py
+++ LRU.py
@@ -2,7 +2,7 @@ class LRU:
     def __init__(self, cache_size):
         self.cache_size = cache_size
         self.cache = []
-    def set(self, num):
+    def put(self, num):
         pass
     def get(self, num):
         pass
---
+++ LRUTest.py
@@ -0,0 +1,11 @@
+from LRU import LRU
+class LRUTest:
+    def __init__(self):
+        self.lru = LRU()
+        
+    def put(self, num):
+        pass
+    def get(self, num):
+        pass
+    def get_cache(self):
+        pass
